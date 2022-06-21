a410858209bde8d3264489de7b4c9c5601e67f4a
Ryan-Hinds <101537819+Ryan-Hinds@users.noreply.github.com>
2022-06-21 16:47:26 -0600

    fixed login and register page and functions

--- finance/app.py
+++ finance/app.py
@@ -1,14 +1,14 @@
 import os
-import sqlite3
-from unittest import result
 
+from unittest import result
+from webbrowser import get
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
-from datetime import datetime, timezone
 
 # Configure application
 app = Flask(__name__)
@@ -26,13 +26,14 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Connect to database
-con = sqlite3.connect("file:finance.db?mode=rw",uri=True, check_same_thread=False)
+# con = sqlite3.connect("file:finance.db?mode=rw",uri=True, check_same_thread=False)
+db = SQL("sqlite:///finance.db")
 # Creates a cursor for the database
-cur = con.cursor()
+#cur = con.cursor()
 # Creates the actual table
-cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 10000.00)")
-cur.execute("CREATE TABLE IF NOT EXISTS orders (id INTEGER, user_id NUMERIC NOT NULL, symbol TEXT NOT NULL, shares NUMERIC NOT NULL, price NUMERIC NOT NULL, timestamp TEXT, PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES users(id))")
-con.commit()
+db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 10000.00)")
+db.execute("CREATE TABLE purchases (id INTEGER, user_id NUMERIC NOT NULL, symbol TEXT NOT NULL, shares NUMERIC NOT NULL, price NUMERIC NOT NULL);")
+# con.commit()
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
@@ -51,36 +52,72 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    
-    owned = shares_owned()
-    total = 0
-    # Iterate over the symbols and shares
-    for symbol, shares in owned.items():
-        result = lookup(symbol)
-        name = result["name"]
-        price = result["price"]
-        stock_value = shares * price
-        total += stock_value
-        owned[symbol] = (name, shares, usd(price), usd(stock_value))
 
-    cash = cur.execute("SELECT cash FROM users WHERE id=?", session["user_id"][0]["cash"])
-    
-    return render_template("index.html", owned=owned, cash=usd(cash, total=usd(total)))
+    user = session["user_id"]
+    total = 0
+
+    # Summary of user's stocks
+    owns = db.execute("SELECT symbol, SUM(shares) AS shares FROM purchases GROUP BY symbol HAVING user_id=? AND SUM(shares) > 0", user)
+
+    # Get's user's current cash balance
+    balance = db.execute("SELECT cash FROM users WHERE id=?",user)[0]["cash"]
+
+    # Retrieves table elements
+    for own in owns:
+        result = lookup(own["symbol"])
+        own["name"] = result["name"]
+        own["price"] = result["price"]
+        own["value"] = own["price"] * own["shares"]
+        total += own["value"]
+    total += balance
+
+    # Returns the index page with the set values from the users profile
+    return render_template("index.html", owns=owns, balance=usd(balance), total=usd(total))
 
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     """Buy shares of stock"""
-    returnusername = request.form.get("username")
-    password =  apology("TODO")
+    if request.method == "POST":
+        # Infor for conditional statements
+        symbol = request.form.get("symbol")
+        result = lookup(symbol)
+        shares = int(request.form.get("shares"))
+        user = session["user_id"]
+        balance = db.execute("SELECT cash FROM users WHERE id=?", user)[0]["cash"]
+        price = result["price"]
+        total = price * shares
+
+        if not symbol:
+            return apology("must prove symbol.", 400)
+        
+        if not result:
+            return apology("Invalid symbol", 400)
+        
+        if shares < 1:
+            return apology("Invalid number of shares.", 400)
+        if balance < total:
+            return apology("Insufficient funds.", 400)
+
+        db.execute("INSERT INTO purchases (user_id, symbol, shares, price) VALUES (?, ?, ?, ?)", user, symbol, shares, total)
+        db.execute("UPDATE users SET cash=? WHERE id=?", (balance -  total), user)
+
+        return redirect("/")
+
+    else:
+        return render_template("buy.html")
 
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    user = session["user_id"]
+
+    owns = db.execute("SELECT symbol, shares,price FROM purchases WHERE user_id=?", user)
+
+    return render_template("history.html", owns=owns)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -107,11 +144,12 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = cur.execute("SELECT * FROM users WHERE username=?;", [username])
+        rows = db.execute("SELECT * FROM users WHERE username=?", username)
 
         # Ensure username exists and password is correct
-        if not rows.fetchall() or not check_password_hash(rows[0]["hash"], {"hash": password}):
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
             return apology("invalid username and/or password", 403)
+        
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -163,12 +201,11 @@ def register():
         elif confirmation != password:
             return apology("Passwords must match.", 400)
 
-        rows = cur.execute("SELECT * FROM users WHERE username=?;", [username])
-        if rows.fetchone() != None:
+        rows = db.execute("SELECT * FROM users WHERE username=?;", username)
+        if len(rows) > 0:
             return apology("Username already exists", 400)
         else:
-            new_user = cur.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", {"username": username, "hash": generate_password_hash(password)})
-            con.commit()
+            new_user = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, generate_password_hash(password))
 
         return redirect("/")
 
@@ -180,22 +217,4 @@ def register():
 def sell():
     """Sell shares of stock"""
     return apology("TODO")
-
-def shares_owned():
-    user_id = session["user_id"]
-    owned = {}
-    query = cur.execute("SELECT symbol, shares, FROM orders WHERE user_id=?", [user_id])
-    for q in query:
-        symbol,shares = q["symbol"], q["shares"]
-        owned["symbol"] = owned.setdefault(symbol, 0) + shares
-    
-    # Filter zero-share stocks
-    owned = {k: v for k,v in owned.items() if v != 0}
-
-    return owned
-
-def timenow():
-    now_utc = datetime.now(timezone.utc)
-
-    return str(now_utc.date()) + " @time " + now_utc.time().strftime("%H:%M:%S")
 
